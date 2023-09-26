d61d866c5950cc572667d55cd96b854f21ca9842
talu <talita.santos96@hotmail.com>
2023-09-26 16:37:09 -0300

    [async] correção no exercício 1

--- exercicios/para-sala/exercicio-1/resolucao.js
+++ exercicios/para-sala/exercicio-1/resolucao.js
@@ -1,21 +1,23 @@
+function raiseSalary(percent){
+    this.salary += (this.salary * percent)
+    console.log(`O novo salário é R$${this.salary}`)
+}
+
 let employee1 = {};
 
 employee1.id = 1,
 employee1.firstName = 'talu',
 employee1.lastName = '',
 employee1.salary = 5000
-employee1.raiseSalary = function raiseSalary(percent){
-    this.salary += (this.salary * percent)
-    return `O novo salário é R$${this.salary}`
-}
+employee1.raiseSalary = raiseSalary
 
 let employee2 = {};
 
 employee2.id = 2,
 employee2.firstName = 'talita',
 employee2.lastName = '',
-employee2.salary = 5000
-employee2.raiseSalary = function raiseSalary(percent){
-    this.salary += (this.salary * percent)
-    return `O novo salário é R$${this.salary}`
-}
+employee2.salary = 57000
+employee2.raiseSalary = raiseSalary
+
+employee1.raiseSalary(0.1)
+employee2.raiseSalary(0.5)
