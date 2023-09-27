e3509a6e993c9093790dab90bafcc297b77cd03e
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-09-26 21:08:35 -0300

    criar function Aluna

---
+++ exercicios/extras/ex-2/resolucao/classes/Aluna.js
@@ -0,0 +1,25 @@
+function Aluna(nome, matricula) {
+  this.nome = nome;
+  this.matricula = matricula;
+  this.notas = [];
+
+  this.adicionarNota = (nota) => {
+    this.notas.push(nota);
+  }
+
+  this.calcularMedia = function() {
+    if(this.notas.length === 0) {
+      return 0;
+    }
+
+    const total = this.notas.reduce((soma, nota) => soma + nota, 0);
+    const media = (total / this.notas.length).toFixed(2);
+    return media;
+  }
+
+  this.exibirInformacoes = function() {
+    console.log(`Nome: ${this.nome} - Matrícula: ${this.matricula} - Média: ${this.calcularMedia()}`);
+  }
+}
+
+module.exports = { Aluna }
