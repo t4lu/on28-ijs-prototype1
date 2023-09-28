59e6b0641c1a4d18efe8c3904ba23db46e154172
talu <talita.santos96@hotmail.com>
2023-09-27 23:42:21 -0300

    Adiciona ajuste nos métodos dos objetos implementados

--- exercicios/para-casa/entrega/Talita Nascimento/Animais.js
+++ exercicios/para-casa/entrega/Talita Nascimento/Animais.js
@@ -1,10 +1,8 @@
-//Parte 1 -> implementação das funções construtoras de cada objeto, e suas propriedades.
-
-function Gato(nomeDoGato, idadeDoGato, corDoGato) {
+function Gato(nomeDoGato, idadeDoGato, corDoGato, ehCastrado) {
     this.nomeDoGato = nomeDoGato;
     this.idadeDoGato = idadeDoGato;
     this.corDoGato = corDoGato;
-    this.ehCastrado = MetodosDosAnimais.castrarAnimal;
+    this.ehCastrado = ehCastrado;
     this.historicoDoGato = new HistoricoMedicoDoAnimal();
 
 }
@@ -13,11 +11,11 @@ Gato.prototype.miar = function miar() {
     console.log(`Miau! Miau! Miau!`)
 }
 
-function Cachorro(nomeDoCachorro, idadeDoCachorro, corDoCachorro, racaDoCachorro) {
+function Cachorro(nomeDoCachorro, idadeDoCachorro, corDoCachorro, ehCastrado, racaDoCachorro) {
     this.nomeDoCachorro = nomeDoCachorro;
     this.idadeDoCachorro = idadeDoCachorro;
     this.corDoCachorro = corDoCachorro;
-    this.ehCastrado = MetodosDosAnimais.castrarAnimal;
+    this.ehCastrado = ehCastrado;
     this.racaDoCachorro = racaDoCachorro;
     this.historicoDoCachorro = new HistoricoMedicoDoAnimal();
 }
@@ -31,73 +29,51 @@ function AnimalExotico(nomeDoAnimal, idadeDoAnimal, corDoAnimal, especieDoAnimal
     this.idadeDoAnimal = idadeDoAnimal;
     this.corDoAnimal = corDoAnimal;
     this.especieDoAnimal = especieDoAnimal;
-    this.ehAdulto = MetodosDosAnimais.ehAdulto;
     this.historicoDoAnimal = new HistoricoMedicoDoAnimal();
 }
 
-function HistoricoMedicoDoAnimal() {
-    this.datasDasConsultas = [].reverse;
-    this.nomesDasVacinas = [];
-    this.procedimentosSubmetidos = [];
+AnimalExotico.prototype.ehAdulto = function ehAdulto(idadeDoAnimal) {
+    let especieDoAnimal = this.especieDoAnimal
 
-    return `consultas: ${this.datasDasConsultas}\nvacinas: ${this.nomesDasVacinas} \nprocedimentos: ${this.procedimentosSubmetidos}`
+    if (especieDoAnimal === "Hamster" && idadeDoAnimal >= 2 ||
+        especieDoAnimal === "Papagaio" && idadeDoAnimal >= 36 ||
+        especieDoAnimal === "Calopsita" && idadeDoAnimal >= 18) {
+        console.log(`É um animal adulto.`)
+    } else {
+        console.log(`Não é um animal adulto.`)
+    }
 }
 
-//Parte 2 -> implementação dos métodos que são pertinentes a todos os animais
-const MetodosDosAnimais = {
-    vacinar: function vacinar(nomeDaVacina) {
-        let vacinasRecebidas = this.nomesDasVacinas;
+function HistoricoMedicoDoAnimal() {
+    this.datasDasConsultas = [];
+    this.nomesDasVacinas = [];
+    this.procedimentosSubmetidos = [];
+}
 
-        //verificar se o parametro de vacinas está vazio
-        if (nomeDaVacina == "") {
-            console.log("Não há histórico de vacinas.")
-        } else {
-            vacinasRecebidas.push(nomeDaVacina)
-            console.log(`Vacina "${nomeDaVacina}" adicionada ao histórico!\nVacinas recebidas: ${vacinasRecebidas}`)
-        }
-    },
+HistoricoMedicoDoAnimal.prototype.vacinar = function vacinar(nomeDaVacina) {
+    let vacinasRecebidas = this.nomesDasVacinas;
 
-    consultar: function consultar(dataDaConsulta) {
-        let primeiraConsulta = this.datasDasConsultas.indexOf(dataDaConsulta);
+    //verificar se o parametro de vacinas está vazio
+    if (nomeDaVacina == "") {
+        console.log("Não há histórico de vacinas.")
+    } else {
+        vacinasRecebidas.push(nomeDaVacina)
+        console.log(`Vacina "${nomeDaVacina}" adicionada ao histórico!\nVacinas recebidas: ${vacinasRecebidas}`)
+    }
+},
+
+HistoricoMedicoDoAnimal.prototype.consultar = function consultar(dataDaConsulta) {
         let consultasRealizadas = this.datasDasConsultas
 
         consultasRealizadas.push(dataDaConsulta)
         console.log(`Data ${dataDaConsulta} adicionada ao histórico!`)
-
-        //checar se é a primeira consulta
-        if (primeiraConsulta === -1) {
-            console.log(`Não existem datas anteriores a ${primeiraConsulta}.`)
-        } else {
-            console.log(`Consultas realizadas: ${this.datasDasConsultas}`)
-        }
     },
 
-    realizarProcedimento: function realizarProcedimento(tipoDeProcedimento, motivoDoProcedimento) {
+HistoricoMedicoDoAnimal.prototype.realizarProcedimento = function realizarProcedimento(tipoDeProcedimento, motivoDoProcedimento) {
         let procedimentosSubmetidos = this.procedimentosSubmetidos;
 
         procedimentosSubmetidos.push(tipoDeProcedimento, motivoDoProcedimento);
         console.log(`Tipo de procedimento realizado: ${tipoDeProcedimento}. Motivo do procedimento: ${motivoDoProcedimento}.`)
-    },
-
-    castrarAnimal: function castrarAnimal(animalCastrado) {
-        if (animalCastrado == true) {
-            console.log("É um animal castrado.")
-        } else {
-            console.log("Não é um animal castrado")
-        }
-    },
- 
-    ehAdulto: function ehAdulto(idadeDoAnimal){
-        let especieDoAnimal = this.especieDoAnimal
-
-        if(especieDoAnimal === "Hamster" && idadeDoAnimal >= 2 || 
-        especieDoAnimal === "Papagaio" && idadeDoAnimal >= 36 ||
-        especieDoAnimal === "Calopsita" && idadeDoAnimal >= 18){
-            console.log(`É um animal adulto.`)
-        } else {
-            console.log(`Não é um animal adulto.`)
-        }
     }
-}
 
 module.exports = { Gato, Cachorro, AnimalExotico, HistoricoMedicoDoAnimal }
