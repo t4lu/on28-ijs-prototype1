e425b23927f0d29afb2b00867bb9b1b885c8c728
talu <talita.santos96@hotmail.com>
2023-09-27 23:43:31 -0300

    atualiza arquivo de testes locais (utilizando console.log)

--- exercicios/para-casa/entrega/Talita Nascimento/testes-locais.js
+++ exercicios/para-casa/entrega/Talita Nascimento/testes-locais.js
@@ -1,11 +1,38 @@
-const { Gato, AnimalExotico } = require('./Animais');
-/*
-const gato = new Gato("Tutu", 24, "branco")
-console.log(gato)
-gato.miar()
-console.log(gato.ehCastrado(true))
-*/
+const { Gato, Cachorro, AnimalExotico } = require('./Animais');
 
-const animalExotico = new AnimalExotico('Roberto', 34, 'marrom', 'Papagaio')
-console.log(animalExotico)
-animalExotico.ehAdulto(34)
+const gato = new Gato("Tutu", 24, "branco", true);
+console.log(gato);
+gato.miar();
+gato.historicoDoGato.vacinar("Raiva");
+gato.historicoDoGato.consultar('2023-09-27');
+gato.historicoDoGato.consultar('2022-09-27');
+gato.historicoDoGato.realizarProcedimento("Exame de sangue", "Rotina");
+
+const cachorro = new Cachorro ("Julio", 42, "Preto", false, "SRD");
+console.log(cachorro);
+cachorro.latir();
+cachorro.historicoDoCachorro.vacinar("Raiva");
+cachorro.historicoDoCachorro.consultar('2023-09-27');
+cachorro.historicoDoCachorro.consultar('2022-09-27');
+cachorro.historicoDoCachorro.realizarProcedimento("Exame de urina", "Rotina");
+
+const calopsita = new AnimalExotico("Roberto", 2, "Amarelo e branco", "Calopsita");
+console.log(calopsita);
+calopsita.historicoDoAnimal.vacinar("");
+calopsita.historicoDoAnimal.consultar('2022-09-27');
+calopsita.historicoDoAnimal.realizarProcedimento("Exame físico","Asa machucada")
+calopsita.ehAdulto(2)
+
+const hamster = new AnimalExotico("Bolinha", 12, "Marrom e branco", "Hamster");
+console.log(hamster);
+hamster.historicoDoAnimal.vacinar("");
+hamster.historicoDoAnimal.consultar('2021-09-27');
+hamster.historicoDoAnimal.realizarProcedimento("Exame de fezes", "Alimentação inadequada (engoliu uma pedra)");
+hamster.ehAdulto(12)
+
+const papagaio = new AnimalExotico("Loro José", 34, "Verde e amarelo", "Papagaio");
+console.log(papagaio)
+papagaio.historicoDoAnimal.vacinar("");
+papagaio.historicoDoAnimal.consultar('2022-09-27');
+papagaio.historicoDoAnimal.realizarProcedimento("Fonoaudiologia", "Está falando pouco");
+papagaio.ehAdulto(34)
